8e5a9e97e352d06e3f9885df040668ef7980feb7
Matthew Downey <matthewdowney20@gmail.com>
2023-04-16 14:27:42 -0400

    Update backprop to support non-linear activation functions

--- src/com/mjdowney/nn_gist.clj
+++ src/com/mjdowney/nn_gist.clj
@@ -55,10 +55,13 @@
 ;              \    /               final layer.
 ;  L_1:        N_1_0
 
-; An activation function and its derivative
-#_(defn sigmoid [z] (/ 1.0 (+ 1.0 (Math/exp (- z)))))
-#_(defn sigmoid' [z] (* (sigmoid z) (- 1 (sigmoid z))))
-(defn sigmoid [z] (max z 0)) ;; TODO
+; The sigmoid (σ) activation function and its derivative
+(defn sigmoid  [z] (/ 1.0 (+ 1.0 (Math/exp (- z)))))
+(defn sigmoid' [z] (* (sigmoid z) (- 1 (sigmoid z))))
+
+; The ReLU (rectified linear unit) activation function and its derivative
+(defn relu  [z] (max z 0))
+(defn relu' [z] (if (pos? z) 1 0))
 
 (defn network
   "Create a network with (deterministically) randomized weights and biases.
@@ -74,26 +77,29 @@
       (partition 2 1 sizes))))
 
 (defn feedforward-layer
-  "Activate each neuron in the layer with the given inputs."
-  [layer inputs]
+  "Activate each neuron in the `layer` with the given `inputs` and activation
+  function `afn`."
+  [layer inputs afn]
   (mapv
     (fn [neuron]
       (let [z (reduce + (:b neuron) (map * (:w neuron) inputs))
-            a (sigmoid z)]
+            a (afn z)]
         (assoc neuron :z z :a a)))
     layer))
 
 (defn feedforward
   "Activate each layer in the network in turn, returning a modified network.
 
+  Takes a series of activation functions `afs` (one per layer).
+
   The return network is shaped [layer], each layer is shaped [neuron], and each
   neuron has an activation value :a and a weighted input value :z."
-  [network inputs]
+  [network inputs afs]
   (loop [idx 0
          inputs inputs
          network network]
     (if (< idx (count network))
-      (let [layer (feedforward-layer (nth network idx) inputs)]
+      (let [layer (feedforward-layer (nth network idx) inputs (nth afs idx))]
         (recur
           (inc idx)
           (mapv :a layer)
@@ -107,17 +113,18 @@
 ; Each neuron changes its own weights, but only *contributes* to desired bias
 ; changes in the previous layer (its desired bias change is summed with the
 ; suggestions from other neurons at the same layer).
-(defn backprop-neuron [neuron error prv-layer]
+(defn backprop-neuron [neuron error prv-layer prv-layer-af']
   (assoc neuron
+    ; From this neuron's perspective, the activations of the previous layer's
+    ; neurons would have to change in this direction.
+    :bg (mapv
+          (fn [w z] (* w error (prv-layer-af' z)))
+          (:w neuron) (map :z prv-layer))
+
     ; Change this neuron's weights in the direction of the error. E.g. if this
     ; neuron's activation needs to increase, increase to a greater degree the
     ; weights corresponding to *more active* neurons in the previous layer.
-    :wg (mapv #(* (:a %) error) prv-layer)
-
-    ; From this neuron's perspective, if the weights were not to change, the
-    ; activations of the previous layers neurons would have to change in this
-    ; direction.
-    :bg (mapv #(* % error) (:w neuron))))
+    :wg (mapv #(* (:a %) error) prv-layer)))
 
 (defn backprop-layer-error [layer]
   ; If the neurons in the previous layer are [n0, n1, ..n], then
@@ -131,10 +138,21 @@
     ; [(+ n0' n0'' ...) etc].
     (apply mapv + per-neuron-perspective)))
 
+(defn network-output-error [network outputs afs']
+  (let [activation-fn-derivative (peek afs')
+        layer (peek network)]
+    (mapv
+      (fn [neuron output]
+        (* (- (:a neuron) output)
+           (activation-fn-derivative (:z neuron))))
+      layer outputs)))
+
 (defn backprop
   "Feed the `inputs` through the `network`, compute the error versus the
   expected `outputs`, and step backwards through the network, estimating
   directional changes to the weights and biases of each layer.
+
+  Takes a series of activation functions `afs` and their derivatives `afs'`.
 
   Returns a map containing the weight and bias gradients (∇w and ∇b), shaped
 
@@ -144,15 +162,17 @@
   The nesting is a tad confusing. The top level vector corresponds to the
   layers, and then the next level to the neurons in each layer. There are
   multiple weights per neuron, hence the triple nesting."
-  [network inputs outputs]
-  (let [network (feedforward network inputs)
+  [network inputs outputs afs afs']
+  (let [network (feedforward network inputs afs)
         input-layer (map (fn [a] {:a a}) inputs)]
     (loop [idx (dec (count network))
            network network
-           error (list (mapv - (mapv :a (last network)) outputs))]
+           error (list (network-output-error network outputs afs'))]
       (if (>= idx 0)
         (let [pl (nth network (dec idx) input-layer)
-              layer (mapv #(backprop-neuron %1 %2 pl)
+              ; activation function for the input layer is f(x) = x
+              af' (nth afs' (dec idx) (constantly 1))
+              layer (mapv #(backprop-neuron %1 %2 pl af')
                       (nth network idx)
                       (first error))]
           (recur
@@ -180,6 +200,7 @@
      [[0.12167502], [0.44386323], [0.33367433], [1.49407907]],
      [[-0.20515826]]])
 
+  ; Network with the weights and biases taken from the Python library
   (def nn
     (vec
       (for [[lws lbs] (map vector weights biases)]
@@ -188,10 +209,16 @@
             {:w nws
              :b nb})))))
 
-  (->> (feedforward nn [3 4]) peek (mapv :a))
+  ; Helper for building vectors of activation functions
+  (defn fns [f] (vec (repeat 3 f)))
+
+  ; Test feedforward result with ReLU
+  (->> (feedforward nn [3 4] (fns relu)) peek (mapv :a))
   ;=> [0.711451179800545]
 
-  (backprop nn [3 4] [5])
+  ; Test backprop result with ReLU (ish, I cheated a bit on the derivative to
+  ; get results with fewer zeros -- doesn't affect testing for consistency)
+  (backprop nn [3 4] [5] (fns relu) (fns (constantly 1)))
   ;=>>
   {:bg [[-13.320990808641826 -0.0531463596480507 -9.090103139068606]
         [-5.276161644216386 -5.156464687149098 1.661069976942607 1.296440101855551]
@@ -204,4 +231,23 @@
          [0.0 0.0 0.6406518503945805]
          [0.0 0.0 0.5000191212342855]]
         [[-0.5974954256335997 -4.333898954159115 -0.0 -6.666037594022326]]]}
+
+  ; Test feedforward result with σ
+  (->> (feedforward nn [3 4] (fns sigmoid)) peek (mapv :a))
+  ;=> [0.7385495823882188]
+
+  ; Test backprop result with σ
+  (backprop nn [3 4] [5] (fns sigmoid) (fns sigmoid'))
+  ;=>
+  {:bg [[-0.04805483794290462 0.003308456819531235 -0.07565334396648868]
+        [-0.24708450430178128 -0.16241027810037614 0.07909991388045877 0.03940968041967344]
+        [-0.8228609192012977]],
+   :wg [[[-0.14416451382871387 -0.19221935177161847]
+         [0.009925370458593704 0.01323382727812494]
+         [-0.22696003189946604 -0.3026133758659547]]
+        [[-0.021846114139705986 -0.006634547316343827 -0.14707552467992419]
+         [-0.014359595244017212 -0.004360931810606212 -0.09667371465695357]
+         [0.0069936629654559785 0.0021239378116470666 0.04708373505242684]
+         [0.00348442885599166 0.0010582022948188068 0.023458368793990856]]
+        [[-0.4748052863127182 -0.6525277264159819 -0.3763548135364772 -0.6604340885279468]]]}
   )

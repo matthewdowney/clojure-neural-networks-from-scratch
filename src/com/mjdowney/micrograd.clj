8cd6404f8edbfafdd593c6b998acaa94623b4194
Matthew Downey <matthewdowney20@gmail.com>
2023-05-03 14:27:48 -0400

    Improve the micrograd implementation

--- src/com/mjdowney/micrograd.clj
+++ src/com/mjdowney/micrograd.clj
@@ -1,111 +1,97 @@
-; Exclude basic arithmetic when including Clojure core:
 (ns com.mjdowney.micrograd
-  (:require [clojure.java.io :as io]
-            [nextjournal.clerk :as clerk]
-            [nextjournal.clerk.viewer :as v]
-            [com.phronemophobic.clj-graphviz :as gv])
-  (:refer-clojure :exclude [+ - * /])
-  (:import (javax.imageio ImageIO)))
+  "autograd engine for scalar values and operations in the style of
 
-{:nextjournal.clerk/visibility {:code :show :result :hide}}
+    https://github.com/karpathy/micrograd
 
-; Create a custom `Value` type that implements the protocol
-(defrecord Value [v op children])
+  but with immutable values.")
 
-(defn -op [this other f]
-  (cond
-    (map? this)
-    (map->Value
-      {:v (f (:v this) (:v other))
-       :op (symbol (name (symbol f)))
-       :children #{this other}})
+; Wrap a scalar `data` with some additional context
+(defrecord Value [data grad children op op-deriv])
 
-    (instance? java.lang.Number this) (f this other)
-    :else (f other this)))
+(defn value
+  "Stick the scalar `n` in a `Value` record."
+  [n]
+  (map->Value {:data n :grad 0.0}))
 
-(defn + [this other] (-op this other #'clojure.core/+))
-(defn - [this other] (-op this other #'clojure.core/-))
-(defn * [this other] (-op this other #'clojure.core/*))
-(defn / [this other] (-op this other #'clojure.core//))
+(defn- operation
+  "Helper for implementing math.
 
-(defn value [v] (->Value v nil #{}))
+  Apply `op` to each of the named values in `env`
 
-{:nextjournal.clerk/visibility {:code :show :result :show}}
+    (calling `(op (get env vn) ...)`)
 
-#_(clerk/row
-  (for [mf [#'clojure.math/sin #'clojure.math/cos #'clojure.math/tan]]
-    (let [f (juxt identity mf)
-          data (map f (range -10 10 0.1))]
-      (clerk/plotly
-        {:data [{:x (map first data) :y (map peek data)}]
-         :layout {:title (str mf) :width 400}}))))
+  and return a new `Value` with the result."
+  [env op op-deriv vn & vns]
+  (let [args (cons vn vns)
+        data (map (comp :data env) args)]
+    (->Value (apply op data) 0.0 args op op-deriv)))
 
-#_{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+; Define some math. Derivative functions take the gradient of the output
+; and the value names for the children of the operation, and return a map of
+; child value name -> gradient.
 
-(def graph-viewer
-  {:pred :graph
-   :transform-fn
-   (clerk/update-val
-     (fn [m]
-       (let [path (str "tmp/graph-" (hash m) ".png")
-             opts (get m :opts {})]
-         (io/make-parents (io/file path))
-         (gv/render-graph (:graph m) (assoc opts :filename path))
-         (clerk/col {::clerk/width :wide}
-           (ImageIO/read (io/file path))))))})
+(defn- mul-deriv [env og [vn1 vn2]]
+  ; look up the child values in the environment
+  {vn1 (-> env vn2 :data (* og))
+   vn2 (-> env vn1 :data (* og))})
 
-(clerk/add-viewers! [graph-viewer])
+(defn- add-deriv [_env og [v0 v1]] {v0 og v1 og})
 
-(defn render-graph [graph & {:keys [] :as opts}]
-  {:graph
-   (merge
-     {:flags #{:directed}
-      :default-attributes {:graph {:rankdir "LR"}}}
-     graph)
-   :opts opts})
+(defn mul "Multiplication." [env v0 v1] (operation env #'* #'mul-deriv v0 v1))
+(defn add "Addition." [env v0 v1] (operation env #'+ #'add-deriv v0 v1))
 
-{:nextjournal.clerk/visibility {:code :show :result :show}}
+(defn- add-gradient
+  "Update the :grad for the value named `vname` by summing in the `delta`."
+  [env vname delta]
+  (update-in env [vname :grad] + delta))
 
-(render-graph {:nodes ["a" "b" "c"]})
+(defn- step-back [env name]
+  (let [v (get env name)]
+    ; If there's a backwards op, call it on the children and update the env
+    ; with the resulting changes, otherwise return env unchanged
+    (if-let [op' (:op-deriv v)]
+      (reduce-kv add-gradient env (op' env (:grad v) (:children v)))
+      env)))
 
-(defn draw* [{:keys [v op children]} points-to nodes edges]
-  (let [add-node (comp (filter some?) (map str))
-        add-edge (comp (filter #(every? some? %)) (map #(mapv str %)))
-        nodes (into nodes add-node [op v])
-        edges (into edges add-edge [[op v] [v points-to]])]
-    (reduce
-      (fn [[nodes edges] child] (draw* child op nodes edges))
-      [nodes edges]
-      children)))
+(defn backwards
+  "Set the gradient of the value named `vn` to 1.0, and then backpropagate
+  through the graph to update the gradients of all other values."
+  [env vn]
+  (loop [q [vn]
+         env (assoc-in env [vn :grad] 1.0)]
+    (if-let [nxt (first q)]
+      (recur (concat (rest q) (-> env nxt :children)) (step-back env nxt))
+      env)))
 
-(defn draw [v]
-  (let [[n e] (draw* v nil #{} #{})]
-    (render-graph {:nodes n :edges e})))
+^:rct/test
+(comment
+  ;; For example, using the test case from https://youtu.be/VMj-3S1tku0?t=5178
 
-(def result (+ (* (value 2.0) (value 5.0)) (value 3.0)))
-(draw result)
+  ; Define a computational graph with the values and operations
+  (def e
+    (as-> {} env
+      (assoc env :a (value -2))
+      (assoc env :b (value 3))
+      (assoc env :d (mul env :a :b))
+      (assoc env :e (add env :a :b))
+      (assoc env :f (mul env :d :e))))
 
+  e
+  ; {:a #micrograd.Value{:data -2, :grad 0.0, :children nil, :op nil, :op-deriv nil},
+  ;  :b #micrograd.Value{:data 3, :grad 0.0, :children nil, :op nil, :op-deriv nil},
+  ;  :d #micrograd.Value{:data -6, :grad 0.0, :children (:a :b), :op #'clojure.core/*, :op-deriv #'micrograd/mul-deriv},
+  ;  :e #micrograd.Value{:data 1, :grad 0.0, :children (:a :b), :op #'clojure.core/+, :op-deriv #'micrograd/add-deriv},
+  ;  :f #micrograd.Value{:data -6, :grad 0.0, :children (:d :e), :op #'clojure.core/*, :op-deriv #'micrograd/mul-deriv}}
 
 
-(def a 2)
-(def b -3)
-(def c 10)
-^{:nextjournal.clerk/no-cache true}
-(def d '(+ (* a b) c))
+  ; Take the gradients with respect to :f
+  (backwards e :f)
+  ; {:a #micrograd.Value{:data -2, :grad -3.0, :children nil, :op nil, :op-deriv nil},
+  ;  :b #micrograd.Value{:data 3, :grad -8.0, :children nil, :op nil, :op-deriv nil},
+  ;  :d #micrograd.Value{:data -6, :grad 1.0, :children (:a :b), :op #'clojure.core/*, :op-deriv #'micrograd/mul-deriv},
+  ;  :e #micrograd.Value{:data 1, :grad -6.0, :children (:a :b), :op #'clojure.core/+, :op-deriv #'micrograd/add-deriv},
+  ;  :f #micrograd.Value{:data -6, :grad 1.0, :children (:d :e), :op #'clojure.core/*, :op-deriv #'micrograd/mul-deriv}}
 
-(def nodes (atom #{}))
-(defn traverse [op & d]
-  (doseq [])
-  (let [[op & args] d
-        args (doall
-               (for [a args]
-                 (if (list? a)
-                   (traverse a)
-                   (let [a+ (eval #p a)
-                         a (str a "=" a+)]
-                     (swap! nodes conj a)
-                     a+))))
-        ret (eval (cons op args))]
-    (swap! nodes c)
-    )
-
+  (update-vals (backwards e :f) :grad)
+  ;=> {:a -3.0, :b -8.0, :d 1.0, :e -6.0, :f 1.0}
+  )
